356b3bd8e955948b049e811a9a705b5003b4821d
Paganelli Giovanni <paganelli.giovanni.97@gmail.com>
2026-02-27 17:58:11 +0100

    added date format constants

--- src/main/webapp/app/config/input.constants.ts
+++ src/main/webapp/app/config/input.constants.ts
@@ -1,2 +1,4 @@
 export const DATE_FORMAT = 'YYYY-MM-DD';
+export const TIME_FORMAT = 'HH:mm';
 export const DATE_TIME_FORMAT = 'YYYY-MM-DDTHH:mm';
+export const DATE_TIME_INPUT_FORMAT = 'YYYY-MM-DDT00:00';
